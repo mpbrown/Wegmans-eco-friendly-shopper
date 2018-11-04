e8fe6401dec1acb8091b2b4f8b3676326c40e66c
Mike Brown <mpbrown@buffalo.edu>
2018-11-04 00:40:52 -0400

    Updated with imports

--- FoodPointTech/app/src/main/java/io/github/anthonymj/foodpointtech/DatabaseHelper.java
+++ FoodPointTech/app/src/main/java/io/github/anthonymj/foodpointtech/DatabaseHelper.java
@@ -1,5 +1,13 @@
 package io.github.anthonymj.foodpointtech;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class DatabaseHelper
 {
 	public static void main(String[] args) 
