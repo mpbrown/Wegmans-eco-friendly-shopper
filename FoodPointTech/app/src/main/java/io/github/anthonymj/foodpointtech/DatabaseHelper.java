934a1b87b21e3095d8b1fc5190eb2f0789728bb2
Mike Brown <mpbrown@buffalo.edu>
2018-11-04 02:19:42 -0500

    Update FoodPointTech/app/src/main/java/io/github/anthonymj/foodpointtech/DatabaseHelper.java

--- FoodPointTech/app/src/main/java/io/github/anthonymj/foodpointtech/DatabaseHelper.java
+++ FoodPointTech/app/src/main/java/io/github/anthonymj/foodpointtech/DatabaseHelper.java
@@ -2,16 +2,23 @@ package io.github.anthonymj.foodpointtech;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class DatabaseHelper
 {
-	private String milkInline = "";
+	private String debugText = "";
 	private int _myresponsecode;
+	private ArrayList<String> cats;
 
 	public DatabaseHelper()
 	 {
@@ -19,14 +26,14 @@ public class DatabaseHelper
 
 		 try {
 			 //milk
-			URL url = new URL("https://api.wegmans.io/products/categories/577-583?api-version=2018-10-18&subscription-key=1474d845d1ef473d9645da89d85320b0");
+			String catid = "577-583";
+			URL url = new URL("https://api.wegmans.io/products/categories/"+catid+"?api-version=2018-10-18&subscription-key=1474d845d1ef473d9645da89d85320b0");
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
 			
 			String inline = "";
 			
 			int responsecode = con.getResponseCode();
-			_myresponsecode = 777;
 			if(responsecode != 200){
 				throw new RuntimeException("HttpResponseCode: "+responsecode);
 			}
@@ -35,24 +42,37 @@ public class DatabaseHelper
 				while(sc.hasNext()){
 					inline+=sc.nextLine();
 				}
-				
-				System.out.println(inline);
-				milkInline = inline;
-				
+
+				JSONParser parse = new JSONParser();
+				JSONObject jobj = (JSONObject)parse.parse(inline);
+				JSONArray prodList = (JSONArray) jobj.get("products");
+				debugText = prodList.get(2);
+
+				for(int i = 0; i < prodList.size(); i++){
+					JSONObject product = (JSONObject) prodList.get(i);
+					System.out.println(product.get("sku")+" is "+product.get("name"));
+
+				}
 				
 			}
+			
+			con.disconnect();
+			
+			URL url2 = 
 			
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			milkInline = e.toString();
+			 e.printStackTrace();
 		} catch (RuntimeException e){
-			 milkInline = e.toString();
-		}
+			 e.printStackTrace();
+		} catch (ParseException e) {
+			 e.printStackTrace();
+		 }
 	 }
 
-	 public String getMilkInline(){
-		return milkInline;
+	 public String getDebugText(){
+		return debugText;
 	 }
 	 public int get_myresponsecode(){
 		return _myresponsecode;
