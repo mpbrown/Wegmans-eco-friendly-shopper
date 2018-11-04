fa7ded91328a1c97e7f234b8ecef2e2f430c8b0b
Anthony Joy <amjoy2@buffalo.edu>
2018-11-03 22:52:12 -0400

    Added views

--- FoodPointTech/app/src/main/java/io/github/anthonymj/foodpointtech/MainActivity.java
+++ FoodPointTech/app/src/main/java/io/github/anthonymj/foodpointtech/MainActivity.java
@@ -1,13 +1,41 @@
 package io.github.anthonymj.foodpointtech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Previous_Visits_Activity.class));
+            }
+        });
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, ShoppingCart.class));
+            }
+        });
+        button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, Expiry.class));
+            }
+        });
     }
 }
---
+++ FoodPointTech/app/src/main/java/io/github/anthonymj/foodpointtech/Previous_Visits_Activity.java
@@ -0,0 +1,13 @@
+package io.github.anthonymj.foodpointtech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Previous_Visits_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous__visits_);
+    }
+}
